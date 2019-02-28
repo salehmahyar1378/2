87a211d2a91f7437aacf14d7a7ac2ea5f6c48522
salehmahyar1378 <42995779+salehmahyar1378@users.noreply.github.com>
2019-02-28 03:50:48 -0800

    Initial commit

---
+++ app/src/main/java/com/example/weight/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.weight;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, sign.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "salam", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
